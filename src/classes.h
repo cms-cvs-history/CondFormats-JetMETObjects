b089df3cf0f135509c3a656fb9cb76a1675326c0
cmsbuild <>
2007-11-13 23:52:41 +0000

    This commit was manufactured by cvs2git to create branch 'CondFormats- JetMETObjects-jetCorrections_1_6_X_retrofit'.
    Sprout from master 2007-11-13 23:52:39 UTC Fedor Ratnikov <sha1-cf070837f75a2e740f971de86d9d2e3a75a81342@cern.ch> 'Generic parameters parser'
    Cherrypick from master 2007-03-31 18:24:17 UTC Fedor Ratnikov <sha1-cf070837f75a2e740f971de86d9d2e3a75a81342@cern.ch> 'Added more user friendly correctionEtEtaPhiP method':
        BuildFile
        src/SimpleMCJetCorrector.cc
        src/classes.h
        src/classes_def.xml

--- src/classes.h
+++ src/classes.h
@@ -1,4 +1,1 @@
 #include "CondFormats/JetMETObjects/interface/SimpleMCJetCorrector.h"
-#include "CondFormats/JetMETObjects/interface/SimpleZSPJetCorrector.h"
-#include "CondFormats/JetMETObjects/interface/Simple3DMCJetCorrector.h"
-#include "CondFormats/JetMETObjects/interface/SimpleL4EMFCorrector.h"
