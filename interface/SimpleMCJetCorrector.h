4d2d074bffe5aad2cb4ebe13efce5131a86d4b45
cmsbuild <>
2008-02-16 00:27:06 +0000

    This commit was manufactured by cvs2git to create tag 'CondFormats- JetMETObjects-CMSSW_1_6_12_pre1'.
    Sprout from master 2008-02-16 00:26:59 UTC Fedor Ratnikov <sha1-cf070837f75a2e740f971de86d9d2e3a75a81342@cern.ch> 'merge 1.6.X version into the main trunk'
    Cherrypick from master 2007-11-17 00:50:14 UTC Fedor Ratnikov <sha1-cf070837f75a2e740f971de86d9d2e3a75a81342@cern.ch> 'L2, L3, and L5 corrections':
        data/CMSSW_120_Iterative_Cone_05.txt
        data/CMSSW_120_Midpoint_Cone_05.txt
        data/CMSSW_120_Midpoint_Cone_07.txt
        data/CSA06_Iterative_Cone_05.txt
        data/CSA06_Midpoint_Cone_05.txt
        data/CSA06_Midpoint_Cone_07.txt
        interface/SimpleMCJetCorrector.h
        src/SimpleL2RelativeCorrector.cc
        src/SimpleL3AbsoluteCorrector.cc
        src/SimpleMCJetCorrector.cc

--- interface/SimpleMCJetCorrector.h
+++ interface/SimpleMCJetCorrector.h
@@ -1,11 +1,13 @@
 //
 // Original Author:  Fedor Ratnikov Dec 27, 2006
-// $Id: SimpleMCJetCorrector.h,v 1.3 2007/03/31 18:24:16 fedor Exp $
+// $Id: SimpleMCJetCorrector.h,v 1.2 2007/03/31 17:32:40 fedor Exp $
 //
 // MC Jet Corrector
 //
 #ifndef SimpleMCJetCorrector_h
 #define SimpleMCJetCorrector_h
+
+#include "CondFormats/JetMETObjects/interface/StandaloneJetCorrector.h"
 
 #include <map>
 #include <string>
@@ -16,7 +18,7 @@ namespace {
   typedef std::map <double, ParametrizationMCJet*> ParametersMap;
 }
 
-class SimpleMCJetCorrector {
+class SimpleMCJetCorrector : public StandaloneJetCorrector {
  public:
   SimpleMCJetCorrector ();
   SimpleMCJetCorrector (const std::string& fDataFile);
@@ -26,7 +28,7 @@ class SimpleMCJetCorrector {
 
   virtual double correctionXYZT (double fPx, double fPy, double fPz, double fE) const;
   virtual double correctionPtEtaPhiE (double fPt, double fEta, double fPhi, double fE) const;
-  virtual double correctionEtEta (double fEt, double fEta) const;
+  virtual double correctionEtEtaPhiP (double fEt, double fEta, double fPhi, double fP) const;
 
  private:
   ParametersMap* mParametrization;
