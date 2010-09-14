cb4b469ac66c1e228ab0a8c000867bc79b35a576
cmsbuild <>
2010-09-14 11:52:33 +0000

    This commit was manufactured by cvs2git to create tag 'CondFormats- JetMETObjects-V01-09-01-11'.
    Sprout from CondFormats-JetMETObjects-jetCorrections_3_6_0_retrofit 2010-09-14 11:52:32 UTC Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch> 'JES uncertainty for the Spring10DataV2 set'
    Cherrypick from CondFormats-JetMETObjects-jetCorrections_3_6_0_retrofit 2010-03-23 16:07:07 UTC cmsbuild 'This commit was manufactured by cvs2git to create branch 'CondFormats-':
        interface/JetCorrectorParameters.h
        src/JetCorrectorParameters.cc
        src/classes.h
        src/classes_def.xml
    Delete:
        data/Spring10_L4EMF_AK5Calo.txt
        data/Spring10_L5Flavor_IC5Calo.txt
        data/Spring10_L7Parton_AK5Calo.txt
        data/Spring10_L7Parton_AK7Calo.txt
        data/Spring10_L7Parton_IC5Calo.txt
        data/Spring10_L7Parton_KT4Calo.txt
        data/Spring10_L7Parton_KT6Calo.txt

--- src/classes.h
+++ src/classes.h
@@ -9,9 +9,4 @@
 JetCorrectorParameters corr;
 JetCorrectorParameters::Definitions def;
 JetCorrectorParameters::Record record;
-std::vector<JetCorrectorParameters> corrv;
 std::vector<JetCorrectorParameters::Record> recordv;
-JetCorrectorParametersCollection coll;
-JetCorrectorParametersCollection::pair_type pair_type;
-JetCorrectorParametersCollection::collection_type colltype;
-std::vector<JetCorrectorParametersCollection> collv;
